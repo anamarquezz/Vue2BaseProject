b97be2a7fbb74c8b38b0a19acdc3a02704548063
ana_marquez <anamarquez.edu@hotmail.com>
2019-04-05 15:05:54 -0700

    Token

--- src/store/actions.js
+++ src/store/actions.js
@@ -7,6 +7,49 @@ import Vue from 'vue'
 export default {
 
 
+  
+
+  _token: function ({
+    commit,
+    state,
+    dispatch
+  }, data) {
+    try{         
+    Vue.http.post("http://localhost:49014/auth/token", {
+        username: data.username,
+        password: "cl0v3r",
+        grant_type: "password",
+        client: "EVALUATION",
+        passwordhind: data.password        
+      },         
+      {
+        emulateJSON: true,
+        progress(e) {
+          if (e.lengthComputable) {           
+             var time = Math.round(e.loaded / e.total ) * 100;
+             commit('s_Loading',{value:time,show:true});  
+          }
+        }, 
+      })
+      .then(responseToken => {
+        var token = "bearer" + " " + responseToken.body.access_token;
+        if (token != "") {
+          commit('set_token', token);
+          dispatch('getEmpleadoInfo');
+        }
+    
+      },
+      response =>{              
+        commit('s_Loading',{value:0,show:false});              
+        dispatch('set_showMessage',{message:response.body == "" ? "Problemas con la conexión a internet" :  response.body.MessageDetail, show:true,title:'Error',showregresar:false,colorThema:'red'});
+        dispatch("gsw_ui", "login");
+      });
+    }catch(e){ 
+      dispatch('set_showMessage', {message:e.message,show:true,title:'Error',showregresar:false,colorThema:'red'});
+     // commit('s_Loading',{value:'100',show:false});
+    }
+  },
+
   init: function ({
     state,
     commit,
